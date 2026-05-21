a0ec7dc45a6ec5a7ef21b96f3b6e59c5692b2005
Ciro Pizza <ciropizza2002@gmail.com>
2026-05-21 22:29:50 +0200

    Update authentication.js

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,1 +1,32 @@
 
+/**
+ * This middleware ensures that the user is currently authenticated. 
+ * If not, redirects to login with an error message.
+*/
+export function enforceAuthentication(req, res, next)
+{
+    if (!req?.session?.isAuthenticated)
+    {
+        req.flash("error", "You must be logged in to access this feature.");
+        res.redirect("/auth");
+    }
+    else
+        next();
+}
+
+
+/**
+ * This middleware makes the authentication status of the user available to templates
+ * by setting the appropriate res.locals properties. 
+ * These properties are used, for example, in the navbar partial template.
+*/
+export function exportAuthenticationStatus(req, res, next)
+{
+	if (req?.session?.isAuthenticated)
+ 	{
+    	res.locals.isUserAuthenticated = true;
+    	res.locals.username = req.session.username;
+  	}
+
+  	next();
+}
