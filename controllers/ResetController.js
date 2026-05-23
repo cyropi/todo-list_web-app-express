d1c8014b2984f249aadfad3ee70e52012e67842a
Ciro Pizza <ciropizza2002@gmail.com>
2026-05-24 00:13:32 +0200

    Update ResetController.js

--- controllers/ResetController.js
+++ controllers/ResetController.js
@@ -1,1 +1,14 @@
 
+import { database } from "../models/Database.js";
+
+
+export class ResetController
+{
+    static async resetApp(req, res)
+    {
+        req.session.username = undefined;
+        req.session.isAuthenticated = false;
+
+        return database.sync({ force: true }); // drop and re-create all tables
+    }
+}
