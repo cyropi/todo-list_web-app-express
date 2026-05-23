e51c499914785866e5b98017a717589834b4b5f7
Ciro Pizza <ciropizza2002@gmail.com>
2026-05-23 22:55:47 +0200

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,1 +1,43 @@
 
+import { User } from "../models/Database.js";
+
+
+export class AuthController
+{
+    static async checkCredentials(req, res) 
+    {
+        let user = new User({ 
+                                userName: req.body.usr, // user data specified in the request
+                                password: req.body.pwd
+                            });
+
+        let found = await User.findOne({ 
+                                           where: 
+                                           {
+                                               userName: user.userName,
+                                               password: user.password // password was hashed when creating user
+                                           }
+                                        });
+
+        if (found === null)
+            return false;
+        else // credentials are valid, create a session
+        {
+            req.session.username = found.userName;
+            req.session.isAuthenticated = true;
+
+            return true;
+        }
+
+    }
+
+
+    static async saveUser(req, res)
+    {
+        let user = new User({ 
+                                userName: req.body.usr, 
+                                password: req.body.pwd
+                            });
+        await user.save();
+    }
+}
